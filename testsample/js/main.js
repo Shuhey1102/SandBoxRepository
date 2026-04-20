b321d67109185e49c2c7769a128f978f7fd3009c
kaji1102 <kajiwara.shuhei.1102kaji@gmail.com>
2026-04-21 02:24:43 +0900

    Add single page layout and placeholder support
    - Created index.html for a single page portfolio layout with sections for About, Works, Skills, and Contact.
    - Included external stylesheets for normalization and custom styles.
    - Added a JavaScript file (placeholders.min.js) for placeholder support in form fields.

--- testsample/js/main.js
+++ testsample/js/main.js
@@ -1,49 +1,27 @@
-const images = [
-  "img1.jpg",
-  "img2.jpg",
-  "img3.jpg",
-  "img4.jpg",
-  "img5.jpg",
-  "img6.jpg"
-];
+const fizzBuzzFunc = () => {
 
-const imgElements = document.querySelectorAll(".slide");
+  let output = ""
+  for (let cnt = 1; cnt<= 100; cnt++){
 
-let imgIndex = 0;
+    output += "No." + cnt + " : "
+    if (cnt % 3 === 0 && cnt % 5 === 0 ){
+      output += "FizzBuzz"
+    }
+    else if(cnt % 3 === 0 ){
+      output += "Fizz"
+    }
+    else if(cnt % 5 === 0 ){
+      output += "Buzz"
+    }
+    else{
+      output += cnt
+    }
+    
+    output += "<br>"
 
-function switchImages() {
-  imgIndex = (imgIndex + 1) % images.length;
-
-  imgElements.forEach((img, index) => {
-    img.src = images[(imgIndex + index) % images.length];
-  });
+  }
+  return output
 }
 
-// 5秒ごとに切替
-setInterval(switchImages, 5000);
 
-
-// ===== 動画切替 =====
-const videos = [
-  "video1.mp4",
-  "video2.mp4",
-  "video3.mp4"
-];
-
-const videoPlayer = document.getElementById("videoPlayer");
-let videoIndex = 0;
-
-function playVideo(index) {
-  videoPlayer.src = videos[index];
-  videoPlayer.load();
-  videoPlayer.play();
-}
-
-// 最初の動画再生
-playVideo(videoIndex);
-
-// 再生終了時に次の動画へ
-videoPlayer.addEventListener("ended", () => {
-  videoIndex = (videoIndex + 1) % videos.length;
-  playVideo(videoIndex);
-});
+document.getElementById("testOutputContent").innerHTML = fizzBuzzFunc()
